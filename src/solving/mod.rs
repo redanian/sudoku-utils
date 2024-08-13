0238781357abd09e4bf599492194edd7414697e4
redanian <>
2024-08-13 10:49:08 +0200

    fix compilation issue

--- src/solving/mod.rs
+++ src/solving/mod.rs
@@ -1,8 +1,9 @@
-mod traits;
-mod set_naked_singles;
-mod eliminate_possibilities_using_existing_singles;
-mod eliminate_possibilities_using_pointing;
-mod eliminate_possibilities_using_naked_pairs;
-mod eliminate_possibilities_using_hidden_groups;
 pub mod solver;
 
+mod eliminate_possibilities_using_existing_singles;
+mod eliminate_possibilities_using_hidden_groups;
+mod eliminate_possibilities_using_naked_pairs;
+mod eliminate_possibilities_using_pointing;
+mod eliminate_possibilities_using_y_wing;
+mod set_naked_singles;
+mod traits;
