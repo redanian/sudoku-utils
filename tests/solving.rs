8543f36aad023bdfd1b8d7789e7e929172d7d946
redanian <>
2024-08-15 18:44:41 +0200

    add medium sudokus to tests

--- tests/solving.rs
+++ tests/solving.rs
@@ -1,6 +1,6 @@
 mod examples;
 
-use examples::EASY_SUDOKUS;
+use crate::examples::{EASY_SUDOKUS, MEDIUM_SUDOKUS};
 use sudoku_utils::{solve, Sudoku};
 
 fn assert_solved_correctly(sudoku: &str, solution: &str) {
@@ -12,7 +12,8 @@ fn assert_solved_correctly(sudoku: &str, solution: &str) {
 
 #[test]
 fn solve_fn_correctly_solves_sudokus() {
-    EASY_SUDOKUS
-        .iter()
+    std::iter::empty()
+        .chain(EASY_SUDOKUS.iter())
+        .chain(MEDIUM_SUDOKUS.iter())
         .for_each(|[sudoku, solution]| assert_solved_correctly(sudoku, solution))
 }
